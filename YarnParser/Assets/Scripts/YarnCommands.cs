c8a135c3ecef9e9f223c02e4025fe22cbaaf6d47
Sammy Chuang <42942569+penguincoco@users.noreply.github.com>
2025-08-03 15:07:04 -0700

    [Added/Updated]
    - TextMeshPro
    - YarnSpinner prefab
    - Demo scene setup using a generated .yarn file and project!

--- YarnParser/Assets/Scripts/YarnCommands.cs
+++ YarnParser/Assets/Scripts/YarnCommands.cs
@@ -1,20 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using Yarn.Unity;
+using Yarn.Unity;
+using UnityEngine.UI;
 
 public class YarnCommands : MonoBehaviour
 {
-    //note: this is commented out because I am too lazy to add the correct version of YarnSpinner to this project right now :) 
-    /*
-    [YarnCommand("initialize_line")]
-    static void SetSpeakerInfo(string info, string info2)
+    [SerializeField] private static Image speaker;
+    [SerializeField] private Image speakerImage;
+
+    [SerializeField] private Sprite frodoSprite;
+    [SerializeField] private Sprite gandalfSprite;
+
+    private static Dictionary<string, Sprite> speakerDictionary = new Dictionary<string, Sprite>();
+
+    private void Awake()
     {
-        string speaker = info;
-        //If not specified emotion, set to neutral
-        string emotion = info2;
-        DialogueAudio.SetDialogueAudio(speaker, emotion);
-        DialogueSpawner.InitializeDialogueBox(speaker);
-        DialoguePortrait.Instance.SetActivePortrait(info, info2);
-    } */
+        speakerDictionary.Add("frodo", frodoSprite);
+        speakerDictionary.Add("gandalf", gandalfSprite);
+
+        speaker = speakerImage;
+    }
+
+    //for documentation on setting up custom commands, callable via .yarn files, see the YarnSpinner documentation
+    //https://docs.yarnspinner.dev/yarn-spinner-for-unity/creating-commands-functions 
+    [YarnCommand("initialize_line")]
+    public static void SetSpeakerInfo(string name, string expression)
+    {
+        speaker.sprite = speakerDictionary[name];
+    }
+
 }
